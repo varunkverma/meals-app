b5bbc736a8fcc7da50e32411cad488e000b2f02d
varunkverma <varunkverma1996@gmail.com>
2020-04-15 09:01:22 +0530

    pushing initial setup and screens files

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+// fetch the fonts from assets
+const fetchFonts = () => {
+  // returns a promise
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
+export default function App() {
+  // state to manage whether fonts are loaded or not
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // if fonts are not loaded, render AppLoading
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.error(err.message)}
+      />
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
